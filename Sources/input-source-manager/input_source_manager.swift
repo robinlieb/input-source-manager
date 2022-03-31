d9a787bc6b0defeec3a4002a061b051495ecd2b2
Robin Lieb <robin.j.lieb@gmail.com>
2022-03-31 16:50:43 +0200

    feat: initialized SPM

---
+++ Sources/input-source-manager/input_source_manager.swift
@@ -0,0 +1,6 @@
+public struct input_source_manager {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/input-source-managerTests/input_source_managerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import input_source_manager
+
+final class input_source_managerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(input_source_manager().text, "Hello, World!")
+    }
+}
