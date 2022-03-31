4c4f04247987efaf4c091eddd565531d1cdc004f
Robin Lieb <robin.j.lieb@gmail.com>
2022-03-31 17:10:49 +0200

    feat: added method for retrieving current keyboard input source id

--- Sources/InputSourceManager/InputSourceManager.swift
+++ Sources/InputSourceManager/InputSourceManager.swift
@@ -1,14 +1,27 @@
 import Foundation
+import Carbon
 
 public protocol InputSourceManaging {
-    
+    func getCurrentKeybaordInputSource() -> String?
 }
 
 public struct InputSourceManager: InputSourceManaging {
+    
     // MARK: - Properties
     public private(set) var text = "Hello, World!"
-
+    
     // MARK: - Init
     public init() {
+    }
+    
+    // MARK: - Methods
+    public func getCurrentKeybaordInputSource() -> String? {
+        let currentInputSource = TISCopyCurrentKeyboardInputSource().takeUnretainedValue()
+        
+        guard let id = TISGetInputSourceProperty(currentInputSource, kTISPropertyInputSourceID) else {
+            return nil
+        }
+        
+        return Unmanaged<AnyObject>.fromOpaque(id).takeUnretainedValue() as? String
     }
 }
