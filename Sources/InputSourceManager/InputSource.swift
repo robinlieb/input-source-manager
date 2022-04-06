bffe9015bbdf95abb515dc743938e07b49a08ce5
Robin Lieb <robin.j.lieb@gmail.com>
2022-04-06 16:20:35 +0200

    docs: added description of properties

--- Sources/InputSourceManager/InputSource.swift
+++ Sources/InputSourceManager/InputSource.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 public struct InputSource {
+    
+    // Identifier of the input source, e.g com.apple.keylayout.US
     public var id: String
+    
+    // Localized name of the input source, e.g. U.S.
     public var localizedName: String
 }
